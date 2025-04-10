0d994500dad5f79e06c129b4d957c728bb9a97c7
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2025-04-10 19:04:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Quran App', headerStyle: { backgroundColor: '#1E1E2C' }, headerTintColor: '#fff' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
